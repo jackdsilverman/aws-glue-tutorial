ccbd4442754e50cde785854d6a3cae59646528d2
Jack Silverman <Jackdsilverman@gmail.com>
2018-08-03 08:51:22 -0400

    updated glue script

--- glue-tutorial-script.py
+++ glue-tutorial-script.py
@@ -8,10 +8,13 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from datetime import datetime
 
-args = getResolvedOptions(sys.argv, ['TempDir', 'TABLE_NAME', 'SCHEMA_NAME', 'REDSHIFT_DB_NAME', 'CATALOG_CONNECTION_NAME'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME','TempDir', 'TABLE_NAME', 'SCHEMA_NAME', 'REDSHIFT_DB_NAME', 'CATALOG_CONNECTION_NAME'])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
 
 datasource = glueContext.create_dynamic_frame.from_catalog(
     database = args['SCHEMA_NAME'],
@@ -65,4 +68,4 @@ datasink = glueContext.write_dynamic_frame.from_jdbc_conf(
     redshift_tmp_dir = args["TempDir"],
     transformation_ctx = "datasink")
 
-
+job.commit()
