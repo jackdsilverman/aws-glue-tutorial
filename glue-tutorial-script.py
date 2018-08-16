2013f6403c0fdd6345f26e4fabe1deaf564d2644
Jack Silverman <Jackdsilverman@gmail.com>
2018-08-16 10:47:54 -0400

    updated script

--- glue-tutorial-script.py
+++ glue-tutorial-script.py
@@ -4,37 +4,34 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from datetime import datetime
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME','TempDir', 'TABLE_NAME', 'SCHEMA_NAME', 'REDSHIFT_DB_NAME', 'CATALOG_CONNECTION_NAME'])
-
+args = getResolvedOptions(sys.argv, ['TempDir', 'JOB_NAME', 'TABLE_NAME', 'SCHEMA_NAME', 'REDSHIFT_DB_NAME', 'CONNECTION_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+job.init(args['JOB_NAME'], args)
+
 datasource = glueContext.create_dynamic_frame.from_catalog(
     database = args['SCHEMA_NAME'],
     table_name = args['TABLE_NAME'],
-    transformation_ctx = "datasource"
-)
+    transforation_ctx = 'datasource')
 
-########## PYSPARK DATAFRAME TRANSFORMS ##########
-
-#Convert to PySpark Data Frame
 sourcedata = datasource.toDF()
 
 split_col = split(sourcedata["quarter"], " ")
 sourcedata = sourcedata.withColumn("quarter new", split_col.getItem(0))
 sourcedata = sourcedata.withColumn("profit", col("revenue")*col("gross margin"))
-sourcedata = sourcedata.withColumn("timestamp", current_date() )
+sourcedata = sourcedata.withColumn("timestamp", current_date())
 
 # Convert back to Glue Dynamic Frame
 datasource = DynamicFrame.fromDF(sourcedata, glueContext, "datasource")
-
 
 applymapping = ApplyMapping.apply(
     frame = datasource,
@@ -53,19 +50,15 @@ applymapping = ApplyMapping.apply(
         ("profit", "double", "profit", "numeric"),
         ("timestamp", "date", "timestamp", "date")
     ], 
-    transformation_ctx = "applymapping"
-)
-
-### datasink (loading) using spark ####
-
+    transformation_ctx = "applymapping")
+# datasink (loading) using spark
 datasink = glueContext.write_dynamic_frame.from_jdbc_conf(
     frame = applymapping,
-    catalog_connection = args['CATALOG_CONNECTION_NAME'],
+    catalog_connection = args['CONNECTION_NAME'],
     connection_options = {
         "dbtable": "{}.{}".format(args['SCHEMA_NAME'], args['TABLE_NAME']),
         "database": args['REDSHIFT_DB_NAME']
-	},
+    },
     redshift_tmp_dir = args["TempDir"],
     transformation_ctx = "datasink")
 
-job.commit()
